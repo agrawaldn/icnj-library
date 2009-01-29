d49fb8d4c276eb8875ca7eb4fa25764885b00d1e
dev.agrawal <dev.agrawal@ae4a3580-e7cb-11dd-80ed-bda8ba2650cd>
2009-01-29 18:11:02 +0000

    cleanup

--- library/database/tables.sql
+++ library/database/tables.sql
@@ -1,14 +1,10 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
-
-CREATE SCHEMA IF NOT EXISTS `krishna` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
-USE `krishna`;
+CREATE SCHEMA IF NOT EXISTS `dagrawal` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
+USE `dagrawal`;
 
 -- -----------------------------------------------------
--- Table `krishna`.`user_role`
+-- Table `user_role`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `krishna`.`user_role` (
+CREATE  TABLE IF NOT EXISTS `user_role` (
   `user_role_id` INT NOT NULL ,
   `user_role` VARCHAR(30) NOT NULL ,
   `updated_by` VARCHAR(30) NOT NULL ,
@@ -16,13 +12,13 @@ CREATE  TABLE IF NOT EXISTS `krishna`.`user_role` (
   PRIMARY KEY (`user_role_id`) )
 ENGINE = InnoDB;
 
-CREATE UNIQUE INDEX `user_role_uk` ON `krishna`.`user_role` (`user_role` ASC) ;
+CREATE UNIQUE INDEX `user_role_uk` ON `user_role` (`user_role` ASC) ;
 
 
 -- -----------------------------------------------------
--- Table `krishna`.`contact`
+-- Table `contact`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `krishna`.`contact` (
+CREATE  TABLE IF NOT EXISTS `contact` (
   `contact_id` INT NOT NULL ,
   `first_name` VARCHAR(30) NOT NULL ,
   `last_name` VARCHAR(30) NULL ,
@@ -38,13 +34,13 @@ CREATE  TABLE IF NOT EXISTS `krishna`.`contact` (
   PRIMARY KEY (`contact_id`) )
 ENGINE = InnoDB;
 
-CREATE UNIQUE INDEX `contact_uk` ON `krishna`.`contact` (`first_name` ASC, `last_name` ASC, `alias` ASC) ;
+CREATE UNIQUE INDEX `contact_uk` ON `contact` (`first_name` ASC, `last_name` ASC, `alias` ASC) ;
 
 
 -- -----------------------------------------------------
--- Table `krishna`.`user`
+-- Table `user`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `krishna`.`user` (
+CREATE  TABLE IF NOT EXISTS `user` (
   `user_id` INT NOT NULL ,
   `user_name` VARCHAR(30) NOT NULL ,
   `contact_id` INT NOT NULL ,
@@ -56,28 +52,28 @@ CREATE  TABLE IF NOT EXISTS `krishna`.`user` (
   PRIMARY KEY (`user_id`) ,
   CONSTRAINT `user_fk`
     FOREIGN KEY (`user_role_id` )
-    REFERENCES `krishna`.`user_role` (`user_role_id` )
+    REFERENCES `user_role` (`user_role_id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `user_fk1`
     FOREIGN KEY (`contact_id` )
-    REFERENCES `krishna`.`contact` (`contact_id` )
+    REFERENCES `contact` (`contact_id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 COMMENT = 'users of the system';
 
-CREATE UNIQUE INDEX `user_uk` ON `krishna`.`user` (`contact_id` ASC) ;
+CREATE UNIQUE INDEX `user_uk` ON `user` (`contact_id` ASC) ;
 
-CREATE INDEX `user_fk` ON `krishna`.`user` (`user_role_id` ASC) ;
+CREATE INDEX `user_fk` ON `user` (`user_role_id` ASC) ;
 
-CREATE INDEX `user_fk1` ON `krishna`.`user` (`contact_id` ASC) ;
+CREATE INDEX `user_fk1` ON `user` (`contact_id` ASC) ;
 
 
 -- -----------------------------------------------------
--- Table `krishna`.`account_type`
+-- Table `account_type`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `krishna`.`account_type` (
+CREATE  TABLE IF NOT EXISTS `account_type` (
   `account_type_id` INT NOT NULL ,
   `account_type` VARCHAR(30) NOT NULL ,
   `max_borrow_limit_nb` INT NULL ,
@@ -86,13 +82,13 @@ CREATE  TABLE IF NOT EXISTS `krishna`.`account_type` (
   PRIMARY KEY (`account_type_id`) )
 ENGINE = InnoDB;
 
-CREATE UNIQUE INDEX `account_type_uk` ON `krishna`.`account_type` (`account_type` ASC) ;
+CREATE UNIQUE INDEX `account_type_uk` ON `account_type` (`account_type` ASC) ;
 
 
 -- -----------------------------------------------------
--- Table `krishna`.`account`
+-- Table `account`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `krishna`.`account` (
+CREATE  TABLE IF NOT EXISTS `account` (
   `account_id` INT NOT NULL ,
   `account_nb` INT NOT NULL ,
   `contact_id` INT NOT NULL ,
@@ -105,27 +101,27 @@ CREATE  TABLE IF NOT EXISTS `krishna`.`account` (
   PRIMARY KEY (`account_id`) ,
   CONSTRAINT `account_fk`
     FOREIGN KEY (`contact_id` )
-    REFERENCES `krishna`.`contact` (`contact_id` )
+    REFERENCES `contact` (`contact_id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `account_fk1`
     FOREIGN KEY (`account_type_id` )
-    REFERENCES `krishna`.`account_type` (`account_type_id` )
+    REFERENCES `account_type` (`account_type_id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE UNIQUE INDEX `account_uk` ON `krishna`.`account` (`account_nb` ASC, `start_datetime` ASC) ;
+CREATE UNIQUE INDEX `account_uk` ON `account` (`account_nb` ASC, `start_datetime` ASC) ;
 
-CREATE INDEX `account_fk` ON `krishna`.`account` (`contact_id` ASC) ;
+CREATE INDEX `account_fk` ON `account` (`contact_id` ASC) ;
 
-CREATE INDEX `account_fk1` ON `krishna`.`account` (`account_type_id` ASC) ;
+CREATE INDEX `account_fk1` ON `account` (`account_type_id` ASC) ;
 
 
 -- -----------------------------------------------------
--- Table `krishna`.`media_type`
+-- Table `media_type`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `krishna`.`media_type` (
+CREATE  TABLE IF NOT EXISTS `media_type` (
   `media_type_id` INT NOT NULL ,
   `media_type` VARCHAR(30) NOT NULL ,
   `description` VARCHAR(60) NULL ,
@@ -134,13 +130,13 @@ CREATE  TABLE IF NOT EXISTS `krishna`.`media_type` (
   PRIMARY KEY (`media_type_id`) )
 ENGINE = InnoDB;
 
-CREATE UNIQUE INDEX `media_type_uk` ON `krishna`.`media_type` (`media_type` ASC) ;
+CREATE UNIQUE INDEX `media_type_uk` ON `media_type` (`media_type` ASC) ;
 
 
 -- -----------------------------------------------------
--- Table `krishna`.`media_category`
+-- Table `media_category`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `krishna`.`media_category` (
+CREATE  TABLE IF NOT EXISTS `media_category` (
   `media_category_id` INT NOT NULL ,
   `category` VARCHAR(30) NOT NULL ,
   `updated_by` VARCHAR(30) NOT NULL ,
@@ -148,13 +144,13 @@ CREATE  TABLE IF NOT EXISTS `krishna`.`media_category` (
   PRIMARY KEY (`media_category_id`) )
 ENGINE = InnoDB;
 
-CREATE UNIQUE INDEX `media_category_uk` ON `krishna`.`media_category` (`category` ASC) ;
+CREATE UNIQUE INDEX `media_category_uk` ON `media_category` (`category` ASC) ;
 
 
 -- -----------------------------------------------------
--- Table `krishna`.`media`
+-- Table `media`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `krishna`.`media` (
+CREATE  TABLE IF NOT EXISTS `media` (
   `media_id` INT NOT NULL ,
   `title` VARCHAR(45) NOT NULL ,
   `media_type_id` INT NOT NULL ,
@@ -171,27 +167,27 @@ CREATE  TABLE IF NOT EXISTS `krishna`.`media` (
   PRIMARY KEY (`media_id`) ,
   CONSTRAINT `media_fk`
     FOREIGN KEY (`media_type_id` )
-    REFERENCES `krishna`.`media_type` (`media_type_id` )
+    REFERENCES `media_type` (`media_type_id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `media_fk1`
     FOREIGN KEY (`media_category_id` )
-    REFERENCES `krishna`.`media_category` (`media_category_id` )
+    REFERENCES `media_category` (`media_category_id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `media_fk` ON `krishna`.`media` (`media_type_id` ASC) ;
+CREATE INDEX `media_fk` ON `media` (`media_type_id` ASC) ;
 
-CREATE UNIQUE INDEX `media_uk` ON `krishna`.`media` (`title` ASC, `media_category_id` ASC) ;
+CREATE UNIQUE INDEX `media_uk` ON `media` (`title` ASC, `media_category_id` ASC) ;
 
-CREATE INDEX `media_fk1` ON `krishna`.`media` (`media_category_id` ASC) ;
+CREATE INDEX `media_fk1` ON `media` (`media_category_id` ASC) ;
 
 
 -- -----------------------------------------------------
--- Table `krishna`.`media_lending`
+-- Table `media_lending`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `krishna`.`media_lending` (
+CREATE  TABLE IF NOT EXISTS `media_lending` (
   `media_lending_id` INT NOT NULL ,
   `media_id` INT NOT NULL ,
   `account_id` INT NOT NULL ,
@@ -203,24 +199,18 @@ CREATE  TABLE IF NOT EXISTS `krishna`.`media_lending` (
   PRIMARY KEY (`media_lending_id`) ,
   CONSTRAINT `media_lending_fk`
     FOREIGN KEY (`account_id` )
-    REFERENCES `krishna`.`account` (`account_id` )
+    REFERENCES `account` (`account_id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `media_lending_fk1`
     FOREIGN KEY (`media_id` )
-    REFERENCES `krishna`.`media` (`media_id` )
+    REFERENCES `media` (`media_id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `media_lending_fk` ON `krishna`.`media_lending` (`account_id` ASC) ;
+CREATE INDEX `media_lending_fk` ON `media_lending` (`account_id` ASC) ;
 
-CREATE INDEX `media_lending_fk1` ON `krishna`.`media_lending` (`media_id` ASC) ;
+CREATE INDEX `media_lending_fk1` ON `media_lending` (`media_id` ASC) ;
 
-CREATE UNIQUE INDEX `media_lending_uk` ON `krishna`.`media_lending` (`media_id` ASC, `account_id` ASC, `lending_datetime` ASC) ;
-	
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+CREATE UNIQUE INDEX `media_lending_uk` ON `media_lending` (`media_id` ASC, `account_id` ASC, `lending_datetime` ASC) ;
