1a4f05533617ca1778f06dd832b6984d5bb85489
dev.agrawal <dev.agrawal@ae4a3580-e7cb-11dd-80ed-bda8ba2650cd>
2009-04-28 21:23:34 +0000

    struts-menu initial integration

---
+++ library/src/com/library/util/DateUtil.java
@@ -0,0 +1,45 @@
+/**
+ * Created by: dagrawal on Apr 20, 2009
+ * Contact: dev.agrawal@gmail.com for help
+ */
+package com.library.util;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class DateUtil {
+	public static Log logger = LogFactory.getLog(DateUtil.class);
+	
+	public static Date addDays(Date date, int days){
+		Calendar c1 = Calendar.getInstance();
+		c1.setTime(date);
+		c1.add(Calendar.DATE, days);
+		return c1.getTime();
+	}
+	public static String getDayofTheDate(Date date) {
+		String day = null;
+		DateFormat f = new SimpleDateFormat("EEEE");
+		try {
+			day = f.format(date);
+		}
+		catch(Exception e) {
+			logger.error("error occured in getDayofTheDate()", e);
+		}
+		return day;
+	}
+
+	public static int daysBetween2Dates(Date d1, Date d2){
+		int days = 0;
+		Calendar c1 = Calendar.getInstance();
+		Calendar c2 = Calendar.getInstance();
+		c1.setTime(d1);
+		c2.setTime(d2);
+		days = (int)(c2.getTime().getTime() - c1.getTime().getTime())/(24*3600*1000);
+		return days;
+	}
+}
